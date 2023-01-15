1795eaa369deb0414ca556f5aae1762d44baabb4
paulodetarso.teles@hotmail.com <potassio@hotmail.com>
2023-01-15 13:08:08 -0300

    Conclusão da paginacao e refatoracao de pastas e arquivos

---
+++ ControleEstoque.Web/Controllers/Cadastro/CadastroGrupoProdutoController.cs
@@ -0,0 +1,89 @@
+﻿using ControleEstoque.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace ControleEstoque.Web.Controllers
+{
+    public class CadastroGrupoProdutoController : Controller
+    {
+        private const int _quantMaxLinhasPorPagina = 10; 
+
+        [Authorize]
+        public ActionResult Index()
+        {
+            ViewBag.ListaTamPag = new SelectList(new int[] { _quantMaxLinhasPorPagina, 20, 30, 40 }, _quantMaxLinhasPorPagina);
+            ViewBag.QuantMaxLinhasPorPagina = _quantMaxLinhasPorPagina;
+            ViewBag.PaginaAtual = 1;
+
+            var lista = GrupoProdutoModel.RecuperarLista(ViewBag.PaginaAtual, _quantMaxLinhasPorPagina);
+            var quant = GrupoProdutoModel.RecuperarQuantidade();
+
+            var difQuantPaginas = (quant % ViewBag.QuantMaxLinhasPorPagina) > 0 ? 1 : 0;
+            ViewBag.QuantPaginas = (quant / ViewBag.QuantMaxLinhasPorPagina) + difQuantPaginas;
+
+            return View(lista);
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public JsonResult GrupoProdutoPagina(int pagina, int tamPag)
+        {
+            var lista = GrupoProdutoModel.RecuperarLista(pagina, tamPag);
+
+            return Json(lista);
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public JsonResult RecuperarGrupoProduto(int id)
+        {
+            return Json(GrupoProdutoModel.RecuperarPeloId(id));
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public JsonResult SalvarGrupoProduto(GrupoProdutoModel model)
+        {
+            string resultado = "Ok";
+            string idSalvo = string.Empty;
+            List<string> mensagens = new List<string>();
+
+            if (!ModelState.IsValid)
+            {
+                resultado = "Aviso";
+                mensagens = ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).ToList(); 
+            }
+            else
+            {
+                try
+                {
+                    int id = model.Salvar();
+
+                    if (id > 0) idSalvo = id.ToString(); 
+                    else resultado = "Erro"; 
+                }
+                catch (Exception)
+                {
+                    resultado = "Erro ao salvar"; 
+                    throw new Exception(resultado);
+                }
+            }
+
+            //Vai ser retornado aqui um objeto anônimo
+            return Json(new { Resultado = resultado, Mensagens = mensagens, IdSalvo = idSalvo});
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public JsonResult ExcluirGrupoProduto(int id)
+        {
+            return Json(GrupoProdutoModel.ExcluirPeloId(id));
+        }
+    }
+}
---
+++ ControleEstoque.Web/Controllers/Cadastro/CadastroUsuarioController.cs
@@ -0,0 +1,72 @@
+﻿using ControleEstoque.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace ControleEstoque.Web.Controllers.Cadastro
+{
+    public class CadastroUsuarioController : Controller
+    {
+        private const string _senhapadrao = "1q2w3e4r";
+
+        [Authorize]
+        public ActionResult Index()
+        {
+            ViewBag.SenhaPadrao = _senhapadrao;
+            return View(UsuarioModel.RecuperarLista());
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult RecuperarUsuario(int id)
+        {
+            return Json(UsuarioModel.RecuperarPeloId(id));
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult SalvarUsuario(UsuarioModel model)
+        {
+            string resultado = "Ok";
+            string idSalvo = string.Empty;
+            List<string> mensagens = new List<string>();
+
+            if (!ModelState.IsValid)
+            {
+                resultado = "Aviso";
+                mensagens = ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).ToList();
+            }
+            else
+            {
+                try
+                {
+                    if (model.Senha == _senhapadrao) model.Senha = "";
+
+                    int id = model.Salvar();
+
+                    if (id > 0) idSalvo = id.ToString();
+                    else resultado = "Erro";
+                }
+                catch (Exception)
+                {
+                    resultado = "Erro ao salvar";
+                    throw new Exception(resultado);
+                }
+            }
+
+            //Vai ser retornado aqui um objeto anônimo
+            return Json(new { Resultado = resultado, Mensagens = mensagens, IdSalvo = idSalvo });
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult ExcluirUsuario(int id)
+        {
+            return Json(UsuarioModel.ExcluirPeloId(id));
+        }
+    }
+}
