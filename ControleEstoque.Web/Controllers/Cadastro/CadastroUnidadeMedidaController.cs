3fa40e4b2ae80154a1662d4447dfe67a1a0abece
paulodetarso.teles@hotmail.com <potassio@hotmail.com>
2023-01-29 10:39:42 -0300

    Criação do Controller Model e View de Unidade de Medida

---
+++ ControleEstoque.Web/Controllers/Cadastro/CadastroUnidadeMedidaController.cs
@@ -0,0 +1,89 @@
+﻿using ControleEstoque.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace ControleEstoque.Web.Controllers.Cadastro
+{
+    public class CadastroUnidadeMedidaController : Controller
+    {
+        private const int _quantMaxLinhasPorPagina = 10;
+
+        [Authorize]
+        public ActionResult Index()
+        {
+            ViewBag.ListaTamPag = new SelectList(new int[] { _quantMaxLinhasPorPagina, 20, 30, 40 }, _quantMaxLinhasPorPagina);
+            ViewBag.QuantMaxLinhasPorPagina = _quantMaxLinhasPorPagina;
+            ViewBag.PaginaAtual = 1;
+
+            var lista = UnidadeMedidaModel.RecuperarLista(ViewBag.PaginaAtual, _quantMaxLinhasPorPagina);
+            var quant = UnidadeMedidaModel.RecuperarQuantidade();
+
+            var difQuantPaginas = (quant % ViewBag.QuantMaxLinhasPorPagina) > 0 ? 1 : 0;
+            ViewBag.QuantPaginas = (quant / ViewBag.QuantMaxLinhasPorPagina) + difQuantPaginas;
+
+            return View(lista);
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public JsonResult UnidadeMedidaPagina(int pagina, int tamPag)
+        {
+            var lista = UnidadeMedidaModel.RecuperarLista(pagina, tamPag);
+
+            return Json(lista);
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public JsonResult RecuperarUnidadeMedida(int id)
+        {
+            return Json(UnidadeMedidaModel.RecuperarPeloId(id));
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public JsonResult SalvarUnidadeMedida(UnidadeMedidaModel model)
+        {
+            string resultado = "Ok";
+            string idSalvo = string.Empty;
+            List<string> mensagens = new List<string>();
+
+            if (!ModelState.IsValid)
+            {
+                resultado = "Aviso";
+                mensagens = ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).ToList();
+            }
+            else
+            {
+                try
+                {
+                    int id = model.Salvar();
+
+                    if (id > 0) idSalvo = id.ToString();
+                    else resultado = "Erro";
+                }
+                catch (Exception)
+                {
+                    resultado = "Erro ao salvar";
+                    throw new Exception(resultado);
+                }
+            }
+
+            //Vai ser retornado aqui um objeto anônimo
+            return Json(new { Resultado = resultado, Mensagens = mensagens, IdSalvo = idSalvo });
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public JsonResult ExcluirUnidadeMedida(int id)
+        {
+            return Json(UnidadeMedidaModel.ExcluirPeloId(id));
+        }
+    }
+}
---
+++ ControleEstoque.Web/Models/UnidadeMedidaModel.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ControleEstoque.Web.Models
+{
+    public class UnidadeMedidaModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nome do grupo é obrigatório!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Sigla obrigatória!")]
+        public string Sigla { get; set; }
+
+        public bool Ativo { get; set; }
+
+        #region Retorna a Quantidade de Grupos
+        public static int RecuperarQuantidade()
+        {
+            int retorno = 0;
+
+            using (var conexao = new SqlConnection())
+            {
+                conexao.ConnectionString = ConfigurationManager.ConnectionStrings["principal"].ConnectionString;
+                conexao.Open();
+
+                using (var comando = new SqlCommand())
+                {
+                    comando.Connection = conexao;
+                    comando.CommandText = "SELECT COUNT(*) FROM unidade_medida";
+                    retorno = (int)comando.ExecuteScalar();
+                }
+                conexao.Close();
+            }
+            return retorno;
+        }
+        #endregion
+
+        #region Recupera a Lista de Grupos
+        public static List<UnidadeMedidaModel> RecuperarLista(int pagina, int tamPagina)
+        {
+            List<UnidadeMedidaModel> retorno = new List<UnidadeMedidaModel>();
+
+            using (var conexao = new SqlConnection())
+            {
+                conexao.ConnectionString = ConfigurationManager.ConnectionStrings["principal"].ConnectionString;
+                conexao.Open();
+
+                using (var comando = new SqlCommand())
+                {
+                    int posicao = (pagina - 1) * tamPagina;
+
+                    comando.Connection = conexao;
+                    comando.CommandText = string.Format(
+                        "SELECT * FROM unidade_medida ORDER BY nome OFFSET {0} ROWS FETCH NEXT {1} ROWS ONLY",
+                        posicao > 0 ? posicao - 1 : 0, tamPagina);
+
+                    var reader = comando.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        retorno.Add(new UnidadeMedidaModel()
+                        {
+                            Id = (int)reader["id"],
+                            Nome = (string)reader["nome"],
+                            Sigla = (string)reader["sigla"], 
+                            Ativo = (bool)reader["ativo"]
+                        });
+                    }
+                }
+                conexao.Close();
+            }
+            return retorno;
+        }
+        #endregion
+
+        #region Recupera o Grupo Pelo ID
+        public static UnidadeMedidaModel RecuperarPeloId(int id)
+        {
+            UnidadeMedidaModel retorno = null;
+            using (var conexao = new SqlConnection())
+            {
+                conexao.ConnectionString = ConfigurationManager.ConnectionStrings["principal"].ConnectionString;
+                conexao.Open();
+                using (var comando = new SqlCommand())
+                {
+                    comando.Connection = conexao;
+                    comando.CommandText = "SELECT * FROM unidade_medida WHERE (id = @id)";
+                    comando.Parameters.Add("@id", SqlDbType.Int).Value = id;
+                    var reader = comando.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        retorno = new UnidadeMedidaModel()
+                        {
+                            Id = (int)reader["id"],
+                            Nome = (string)reader["nome"],
+                            Sigla = (string)reader["sigla"], 
+                            Ativo = (bool)reader["ativo"]
+                        };
+                    }
+                }
+                conexao.Close();
+            }
+            return retorno;
+        }
+        #endregion
+
+        #region Salvar o Grupo de Produto
+        public int Salvar()
+        {
+            int retorno = 0;
+            var model = RecuperarPeloId(this.Id);
+
+            using (var conexao = new SqlConnection())
+            {
+                conexao.ConnectionString = ConfigurationManager.ConnectionStrings["principal"].ConnectionString;
+                conexao.Open();
+                using (var comando = new SqlCommand())
+                {
+                    comando.Connection = conexao;
+                    if (model == null)
+                    {
+                        comando.CommandText = "INSERT INTO unidade_medida (nome, sigla, ativo) VALUES (@nome, @sigla, @ativo); " +
+                                              "SELECT CONVERT(int, SCOPE_IDENTITY());";
+                        comando.Parameters.Add("@nome", SqlDbType.VarChar).Value = this.Nome;
+                        comando.Parameters.Add("@sigla", SqlDbType.VarChar).Value = this.Sigla;    
+                        comando.Parameters.Add("@ativo", SqlDbType.Bit).Value = (this.Ativo ? 1 : 0);
+
+                        retorno = (int)comando.ExecuteScalar();
+                    }
+                    else
+                    {
+                        comando.CommandText = "UPDATE unidade_medida SET nome = @nome, sigla = @sigla, ativo = @ativo WHERE id = @id;";
+                        comando.Parameters.Add("@id", SqlDbType.Int).Value = this.Id;
+                        comando.Parameters.Add("@nome", SqlDbType.VarChar).Value = this.Nome;
+                        comando.Parameters.Add("@sigla", SqlDbType.VarChar).Value = this.Sigla;
+                        comando.Parameters.Add("@ativo", SqlDbType.Bit).Value = (this.Ativo ? 1 : 0);
+                        if (comando.ExecuteNonQuery() > 0) retorno = this.Id;
+                    }
+                }
+                conexao.Close();
+            }
+            return retorno;
+        }
+        #endregion
+
+        #region Excluir Grupo de Produto
+        public static bool ExcluirPeloId(int id)
+        {
+            bool retorno = false;
+            if (RecuperarPeloId(id) != null)
+            {
+                using (var conexao = new SqlConnection())
+                {
+                    conexao.ConnectionString = ConfigurationManager.ConnectionStrings["principal"].ConnectionString;
+                    conexao.Open();
+                    using (var comando = new SqlCommand())
+                    {
+                        comando.Connection = conexao;
+                        comando.CommandText = "DELETE FROM unidade_medida WHERE (id = @id)";
+                        comando.Parameters.Add("@id", SqlDbType.Int).Value = id;
+                        retorno = (comando.ExecuteNonQuery()) > 0;
+                    }
+                    conexao.Close();
+                }
+            }
+            return retorno;
+        }
+        #endregion
+    }
+}
